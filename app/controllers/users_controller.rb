3a6e11b647defea0b9c269a2f2e50579ae0efa31
Prajwal Joshi <prazwal@outlook.com>
2016-04-27 23:53:31 +1000

    install bootstrap, bicrypt gem , signup system in rails

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,65 @@
+class UsersController < ApplicationController
+
+  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_admin, only: [:destroy]
+
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+        flash[:success] = "welcome to JS-haps blog #{@user.username}."
+        redirect_to user_path(@user)
+    else
+      flash[:danger] ="something went wrong"
+      render'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Account was updated successfully"
+      redirect_to articles_path
+    else
+      flash[:danger] ="something went wrong"
+      render 'edit'
+    end
+  end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      flash[:danger] = "User and all articles created by them has been deleted."
+      redirect_root_path
+    end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:danger] = "You can only edit your account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? && !current_user.admin?
+      flash[:danger] = "only admin users can perform this action"
+      redirect_to root_path
+    end
+  end
+
+end
