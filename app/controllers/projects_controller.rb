26c9a2aa3bbc5a78bff58cba1d37e1adfaee6c18
Prajwal Joshi <prazwal@outlook.com>
2016-05-22 01:33:48 +1000

    new projects have default lists

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,9 +14,46 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
+
+    #add default lists to new project
+
+
+
+
+
+
     @project.project_owner_id = current_user.id
     @project.users << current_user
     if @project.save
+      binding.pry
+      list = TaskList.new
+      list.title = 'in progress'
+      list.project_id = @project.id
+      list.task_list_owner_id = current_user
+      list.position = 1
+      list.save
+
+      list = TaskList.new
+      list.title = 'todo'
+      list.project_id = @project.id
+      list.task_list_owner_id = current_user
+      list.position = 2
+      list.save
+
+      list = TaskList.new
+      list.title = 'done'
+      list.project_id = @project.id
+      list.task_list_owner_id = current_user
+      list.position = 4
+      list.save
+
+      list = TaskList.new
+      list.title = 'backlog'
+      list.project_id = @project.id
+      list.task_list_owner_id = current_user.id
+      list.position = 3
+      list.save
+
       redirect_to root_path
     end
   end
